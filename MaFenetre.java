6a8d86c1f686cf0cf4a87bbded7c9cc18adf1332
dmadjib <102877505+dmadjib@users.noreply.github.com>
2024-05-21 23:43:25 +0200

    the paddle moves horizontally with the mouse

--- MaFenetre.java
+++ MaFenetre.java
@@ -1,55 +1,37 @@
 import java.awt.Color;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseMotionAdapter;
-import java.awt.event.MouseMotionListener;
-import java.net.http.WebSocket.Listener;
+import javax.swing.*;
 import java.util.ArrayList;
-import javax.swing.* ;
-
 
 public class MaFenetre extends JFrame {
-    JPanel pan ;
+    JPanel pan;
     Color couleur1 = Color.RED;
     Color couleur2 = Color.ORANGE;
     Color couleur3 = Color.GREEN;
     Color couleur4 = Color.YELLOW;
 
-    //pour définir la taille de la fenêtre
-    private int width,height;
-    
-     //taille prédéfinie : 	(320,200+50) 
-    MaFenetre(int _width,int _height){
-    this.addMouseMotionListener(new MouseMotionAdapter() {
-        public void mouseMoved(MouseEvent e){
-            handlekeypress(e);
-        }
-    });
-     this.width = _width;
-     this.height = _height;
-    
-     setSize(width,height);
-     setTitle(Constants.TITLE);
-     
-     ArrayList listRed = new ArrayList<Brick>();
-     ArrayList listOrange = new ArrayList<Brick>();
-     ArrayList listGreen = new ArrayList<Brick>();
-     ArrayList listYellow = new ArrayList<Brick>();
-     MovingObject une_balle =new Balle(Constants.XinitBal,Constants.YinitBal,Constants.vxinitBal,Constants.vyinitBal);
-     MovingObject un_paddle =new Paddle(Constants.WidthFen/2,Constants.HeightFen-Constants.margeB-Constants.HeightPad);
+    // pour définir la taille de la fenêtre
+    private int width, height;
 
+    // taille prédéfinie : (320,200+50)
+    MaFenetre(int _width, int _height) {
+        this.width = _width;
+        this.height = _height;
 
-     pan = new Paneau(une_balle,un_paddle) ;
- 
-     setContentPane(pan) ;
-    }
- 
-    private void handlekeypress(MouseEvent e) {
-        System.out.println("salut");
-        System.out.println(e);
+        setSize(width, height);
+        setTitle(Constants.TITLE);
+
+        ArrayList<Brick> listRed = new ArrayList<>();
+        ArrayList<Brick> listOrange = new ArrayList<>();
+        ArrayList<Brick> listGreen = new ArrayList<>();
+        ArrayList<Brick> listYellow = new ArrayList<>();
+        MovingObject une_balle = new Balle(Constants.XinitBal, Constants.YinitBal, Constants.vxinitBal, Constants.vyinitBal);
+        Paddle un_paddle = new Paddle(Constants.WidthFen / 2, Constants.HeightFen - Constants.margeB - Constants.HeightPad);
+
+        this.addMouseMotionListener(un_paddle);
+
+        pan = new Paneau(une_balle, un_paddle);
+        setContentPane(pan);
     }
 
- }
- 
+
+}
